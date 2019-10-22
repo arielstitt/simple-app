8f7b349018d7bb5ee10c5fe9b7efdcaae90b4204
Ariel Stitt <arielstitt@Skills-Macbook-Pro-37.local>
2019-10-22 11:15:36 -0400

    added mongoDB URI and set up port

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://as123:stitt123@cluster0-fygxd.mongodb.net/test?retryWrites=true&w=majority'
+}
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+//Bodyparser middleware
+app.use(bodyParser.json());
+
+//DB config
+
+const db = require('./config/keys').mongoURI;
+
+//connect connect to mongo
+
+mongoose
+    .connect(db)
+    .then(() => console.log("mongoDB connected..."))
+    .catch(err => console.log(err))
+    //connect to heroku later
+    const port = process.env.PORT || 5000
+
+    app.listen(port, () => console.log(`server started on port ${port}`));
